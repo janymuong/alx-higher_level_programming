e45ca03f52d42bcbb27c5dcb4da1737acf373eaa
Jany Muong <me.roumuong@gmail.com>
2023-03-08 01:09:29 +0300

    [exe]print ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    if i % 2 == 1:
-        print("{:c}".format(i), end='')
-    else:
-        print("{:c}".format(i - 32), end='')
+for c in range(122, 96, -1):
+    if c % 2 == 1:
+        c -= 32
+    print("{:c}".format(c), end='')
