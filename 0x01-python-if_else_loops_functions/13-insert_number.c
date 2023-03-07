431640005540037187d9c3969b852da3c31a4e39
Jany Muong <me.roumuong@gmail.com>
2023-03-08 00:49:13 +0300

    insert number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,24 +11,39 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current_node;
-
-	current_node = *head;
+	listint_t *current_node, *new_node;
 
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t *));
 
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->next = NULL;
 	new_node->n = number;
-	if (!current_node || current_node->n >= number)
+
+	current_node = *head;
+	while (current_node)
 	{
-		new_node->next = current_node;
-		*head = new_node;
-		return (new_node);
+		if (current_node->n >= number)
+		{
+			new_node->next = current_node;
+			*head = new_node;
+			return (new_node);
+		}
+		else if (current_node->n <= number && current_node->next->n >= number)
+		{
+			if (current_node->next)
+			{
+				new_node->next = current_node->next;
+				current_node->next = new_node;
+				return (new_node);
+			}
+		}
+		current_node = current_node->next;
 	}
-	return (new_node);
+
+	return (NULL);
 }
