d2882ca761a27e5e178043c9df68570e0779ae90
Jany Muong <me.roumuong@gmail.com>
2023-03-09 20:15:05 +0300

    edit out module in function

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,9 +2,9 @@
 def magic_calculation(a, b):
     import magic_calculation_102
     if a < b:
-        c = magic_calculation_102.add(a, b)
+        c = add(a, b)
         for i in range(4, 7):
-            c = magic_calculation_102.add(c, i)
+            c = add(c, i)
         return c
     else:
         return magic_calculation_102.sub(a, b)
