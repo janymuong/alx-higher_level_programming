5c18b82ccc0e259b50947a0252e0800fa594d6bc
Jany Muong <me.roumuong@gmail.com>
2023-03-09 20:19:14 +0300

    return statement

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    import magic_calculation_102
+    from magic_calculation_102
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
         return c
     else:
-        return magic_calculation_102.sub(a, b)
+        return sub(a, b)
