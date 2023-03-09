f97acbcd0fa50f853c3da1effe8bdb7eafff4ff3
Jany Muong <me.roumuong@gmail.com>
2023-03-09 20:24:57 +0300

    range

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     import magic_calculation_102
     if a < b:
         c = magic_calculation_102.add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = magic_calculation_102.add(c, i)
         return c
     else:
