7f59c43977105dbb9448dd233cca70fb23051b29
Jany Muong <me.roumuong@gmail.com>
2023-03-15 20:55:10 +0300

    use map() function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     '''compute the square value of all integers of a matrix'''
 
-    return [[row[i] ** 2 for row in matrix] for i in range(len(matrix))]
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
