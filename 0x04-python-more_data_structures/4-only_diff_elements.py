846e0ce1a1205e7cb1d7ae7ad03fd5485627fdcd
Jany Muong <me.roumuong@gmail.com>
2023-03-15 22:01:00 +0300

    [use multi-line docstring] diff - set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    ''' returns a set of all elements present in only one set - set difference'''
+    '''
+    returns a set of all elements present
+    in only one set - set difference
+    '''
 
     return set_1 ^ set_2
