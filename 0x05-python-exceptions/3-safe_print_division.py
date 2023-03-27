1ec39949da28c5c48673da030245af4015ba3e98
Jany Muong <me.roumuong@gmail.com>
2023-03-27 21:58:15 +0300

    [with execute permission] int-div with debug info

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    '''divide two integers and prints the result'''
+    '''Divide two integers and prints the result'''
 
     try:
         quotient = a / b
