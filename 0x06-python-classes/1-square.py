8b64f1bc1fb4ec55fb64a947032aac9b61cc2001
Jany Muong <me.roumuong@gmail.com>
2023-03-28 19:11:48 +0300

    class with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''class that can be instantitiated'''
+
+
+class Square():
+    '''class that defines a square with an attribute'''
+
+    def __init__(self, __size):
+        self.size = __size
