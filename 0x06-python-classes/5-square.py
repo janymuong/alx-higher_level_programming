97b1a29474072babec750f4e426fe3906c899c28
Jany Muong <me.roumuong@gmail.com>
2023-03-28 21:21:57 +0300

    print area - manipulating attributes data

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''class that can be instantitiated'''
+
+
+class Square():
+    '''class that defines a square with optional size'''
+
+    def __init__(self, size=0):
+        '''
+        Initialization
+        Args:
+            size: the size of the square -private instance attribute
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''Getter method to retrieve attribute'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        Setter method
+        Args:
+            value: the size of the square -private instance attribute
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''return the current square area'''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''print in stdout the square with the character #'''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
