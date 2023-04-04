41d95ffd863e0a47028cd0ab296c7a2fe515fda2
Jany Muong <me.roumuong@gmail.com>
2023-04-04 18:58:02 +0300

    prevent the usr from dynamically creating new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+'''class LockedClass with no class or object attribute'''
+
+
 class LockedClass:
+    '''
+    prevents the user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name
+    '''
     __slots__ = ('first_name',)
 
     def __init__(self):
