9c8ecc634c2d31b1bb28fa970af7ce1b1a44ab95
Jany Muong <me.roumuong@gmail.com>
2023-04-05 15:51:25 +0300

    cover lots of cases & edge cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,7 +4,6 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
     '''TestCase - tests for the function max_integer:
     Function to find and return the max integer in a list of integers
@@ -12,10 +11,13 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         '''unit tests'''
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1, 6, 3, 5]), 6)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([1]), 1)
         self.assertIsNone(max_integer([]))
         self.assertEqual(max_integer([1, 2.2, 3, 4.4]), 4.4)
+        self.assertEqual(max_integer([1, 4, 3, -4]), 4)
         with self.assertRaises(TypeError):
             max_integer([1, 2, 3, "four"])
         self.assertEqual(max_integer(), None)
