0fd686c0394df3b6836533e515ec12d65b967341
Jany Muong <me.roumuong@gmail.com>
2023-04-11 21:52:23 +0300

    class with a public method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module: code for base geometry
+based on 5-base_geometry.py
+'''
+
+
+class BaseGeometry():
+    '''
+    Raises an Exception with a message
+    '''
+
+    def area(self):
+        raise Exception('area() is not implemented')
