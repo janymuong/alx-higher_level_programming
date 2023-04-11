e00f41ece2bc2b5113423bb15601024f32b2d6fb
Jany Muong <me.roumuong@gmail.com>
2023-04-11 17:54:44 +0300

    class MyInt inherits from the class: int(type int)

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module for:
+Class `MyInt` that inherits(uses) `int` as base class.
+'''
+
+
+class MyInt(int):
+    '''
+    Class that inherits props of the int class
+    but with inverted == and != operators.
+    '''
+
+    def __invert_eq__(self, other):
+        '''Inverted == operator'''
+        return int(self) != int(other)
+
+    def __not_eq__(self, other):
+        '''Inverted != op'''
+        return int(self) == int(other)
