e3a0098b977d312be6077e52711704a02283cacd
Jany Muong <me.roumuong@gmail.com>
2023-04-11 22:53:26 +0300

    class with a public class method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Module: code for base geometry
+based on 6-base_geometry.py
+'''
+
+
+class BaseGeometry():
+    '''
+    Base class with methods for basic geometry
+    '''
+    def area(self):
+        '''Method to calculate the area of a geometric shape'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Method to validate the value of an integer
+        '''
+
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
