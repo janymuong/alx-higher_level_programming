a821f6dc095bd2dd374e4680b2ed1f1731403bba
Jany Muong <me.roumuong@gmail.com>
2023-04-12 23:21:22 +0300

    json.dumps(): JSON repr of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module: JSON.dumps'''
+
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    JSON representation of an object (string)
+    '''
+    return json.dumps(my_obj)
