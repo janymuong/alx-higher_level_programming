6617f65bcbe9db444a8102b4dc872ef50844de6c
Jany Muong <me.roumuong@gmail.com>
2023-04-17 15:48:36 +0300

    serialization: JSON string representation of a list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-'''Module: code for base class: source for subclassing
+'''Module: code for base class - source for subclassing
 '''
 
+import json
 
-class Base():
+
+class Base:
     '''
-    Base class: in progress...
+    Base class: represents the base of all other classes
+
+    attributes:
+        __nb_objects: private class attribute
     '''
     __nb_objects = 0
 
@@ -16,3 +21,13 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''static method - for serialization
+        returns the JSON string representation of a list of dictionaries
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
