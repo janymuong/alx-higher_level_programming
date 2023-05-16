3cbf33f68831663c1bdd91516a8187c40c8d877d
Jany Muong <me.roumuong@gmail.com>
2023-05-16 23:52:16 +0300

    [correct]converts to UTF8: utf8mb4, collate utf8mb4_unicode_ci

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,9 +3,5 @@
 -- convert the DB to UTF8
 USE `hbtn_0c_0`;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- convert the table first_table to UTF8
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- convert the name field to UTF8
-ALTER TABLE `first_table` MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
