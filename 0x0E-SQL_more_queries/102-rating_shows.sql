948e1873935600d83faadab7809cf52ef1720f38
Jany Muong <me.roumuong@gmail.com>
2023-05-18 02:10:08 +0300

    rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,8 @@
 -- MySQL: lists all shows from hbtn_0d_tvshows_rate by their rating; sort in desc order by title
 
+-- use the not hbtn_0d_tvshows_rate database
+USE `hbtn_0d_tvshows`;
+
 SELECT t.title, SUM(r.rating) AS rating
 FROM tv_shows AS t
 JOIN rating AS r ON r.show_id = t.id
