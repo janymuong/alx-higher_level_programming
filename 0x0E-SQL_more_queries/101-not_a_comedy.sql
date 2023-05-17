5c2e4a1b1f02acde22c864a664ea33bebdf27f75
Jany Muong <me.roumuong@gmail.com>
2023-05-18 01:52:35 +0300

    distinct joins

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,4 +6,4 @@ FROM `tv_shows` AS t
 LEFT JOIN `tv_show_genres` AS s ON s.`show_id` = t.`id`
 LEFT JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
 WHERE g.`name` != "Comedy" OR g.`name` IS NULL
-ORDER BY t.`title`;
+ORDER BY t.`title` ASC;
