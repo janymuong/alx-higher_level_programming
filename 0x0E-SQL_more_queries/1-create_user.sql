a654a43a8d2da47735dc9ca67241f50744eafd07
Jany Muong <me.roumuong@gmail.com>
2023-05-17 20:57:54 +0300

    root user: create a MySQL server usr; grant ALL privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- MySQL:  a script that creates the MySQL server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+-- MySQL:  a script that creates the MySQL server user; garnt ALL privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
