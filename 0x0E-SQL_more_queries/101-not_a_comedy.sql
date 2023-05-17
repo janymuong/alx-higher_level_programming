ba20dd9ebb7ec9a643977d62a2f20766fabcdb6a
Jany Muong <me.roumuong@gmail.com>
2023-05-18 01:41:58 +0300

    distinct joins

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,15 +1,9 @@
 -- MySQL: a script that lists all shows without the genre Comedy in the database
 
 -- list shows without the genre Comedy
-SELECT title
-FROM `tv_shows`
-WHERE id NOT IN (
-    SELECT show_id
-    FROM `tv_show_genres`
-    WHERE genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-        )
-)
-ORDER BY title;
+SELECT DISTINCT t.`title`
+FROM `tv_shows` AS t
+LEFT JOIN `tv_show_genres` AS s ON s.`show_id` = t.`id`
+LEFT JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
+WHERE g.`name` != "Comedy" OR g.`name` IS NULL
+ORDER BY t.`title`;
