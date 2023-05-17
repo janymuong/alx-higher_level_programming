f38baff9ab3f93405231ec6a2144107268797d4b
Jany Muong <me.roumuong@gmail.com>
2023-05-18 01:31:11 +0300

    conditional WHERE; NOT IN

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- 101-not_a_comedy.sql: a script that lists all shows without the genre Comedy in the database
+
+-- list shows without the genre Comedy
+SELECT title
+FROM `tv_shows`
+WHERE id NOT IN (
+    SELECT show_id
+    FROM `tv_show_genres`
+    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
+)
+ORDER BY title;
