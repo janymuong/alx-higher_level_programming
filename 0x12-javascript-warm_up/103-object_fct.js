f5967a31e56afe68645a517eefbddd09bfbe7f32
Jany Muong <me.roumuong@gmail.com>
2023-06-13 01:48:09 +0300

    maniplulate DOM object props

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// this [keyword]: manipulate object props
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+myObject.incr = function() {
+  this.value++;
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
