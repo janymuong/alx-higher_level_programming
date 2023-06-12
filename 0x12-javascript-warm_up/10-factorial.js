eb16b80ff6e6f913cdf5c89140e3b7edb1843a21
Jany Muong <me.roumuong@gmail.com>
2023-06-13 01:05:35 +0300

    [correct] factorial: recursion, compute factorial(integer)

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
-const factorial = (argInt) => {
-  if (isNaN(argInt)) {
+function factorial (n) {
+  if (isNaN(n)) {
     return 1;
   }
 
-  if (argInt === 0 || argInt === 1) {
+  if (n <= 1) {
     return 1;
   }
 
-  return argInt * factorial(argInt - 1);
-};
+  return n * factorial(n - 1);
+}
 
 const argInt = parseInt(process.argv[2]);
+
 const result = factorial(argInt);
 console.log(result);
