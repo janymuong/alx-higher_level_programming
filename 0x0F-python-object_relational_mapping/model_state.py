d95cfcd3151ba728c3442a2aa163276ef8d95b5f
Jany Muong <me.roumuong@gmail.com>
2023-06-17 15:21:35 +0300

    [rm connection variables] ORM model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,13 +4,8 @@ module ORM: SQLAlchmey
 link class to table in database
 '''
 
-import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
 # declarative_base() function sets up the necessary Base for mapping
 Base = declarative_base()
@@ -25,8 +20,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == '__main__':
-    # creating the database tables based on the SQLAlchemy model
-    Base.metadata.create_all(engine)
