4d597ab898cf79c7d5b74843e777bfc5d9981ec3
Jany Muong <me.roumuong@gmail.com>
2023-06-30 08:57:51 -0700

    curl body size in bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# script takes a URL as input, sends a request to that URL using curl,
-#   and displays the size of the response body in bytes
-
-URL="$1"
-
-# send a request to the URL
-response=$(curl -sI "$URL")
-
-# extract the content length from the response headers
-echo "$response" | grep -i 'Content-Length' | awk '{print $2}'
+# curl for Content-Length of the response body in bytes
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
