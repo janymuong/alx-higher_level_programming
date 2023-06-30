c5033fb1886210cb4d82c429eaefe32db60af37e
Jany Muong <me.roumuong@gmail.com>
2023-06-30 23:50:54 +0300

    [run as the main program] fetch in response header X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,10 +7,9 @@ found in the header of the response.
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
 
 if __name__ == '__main__':
+    url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         x_request_id = res.headers.get('X-Request-Id')
