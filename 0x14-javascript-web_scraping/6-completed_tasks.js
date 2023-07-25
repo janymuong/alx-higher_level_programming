6eda5872580fe433fdcd4fd3ca5888bf0f1d20fb
Jany Muong <me.roumuong@gmail.com>
2023-07-25 17:43:19 +0300

    scrape API url

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+// computes the number of tasks completed by user id; parse data:
+// API URL: https://jsonplaceholder.typicode.com/todos
+
+const req = require('request');
+
+function todosComplete (apiUrl) {
+  req(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const tasks = JSON.parse(body);
+      const completedTasks = {};
+
+      tasks.forEach((task) => {
+        if (task.completed) {
+          if (completedTasks[task.userId]) {
+            completedTasks[task.userId]++;
+          } else {
+            completedTasks[task.userId] = 1;
+          }
+        }
+      });
+
+      console.log(completedTasks);
+    }
+  });
+}
+
+const apiUrl = process.argv[2];
+
+todosComplete(apiUrl);
