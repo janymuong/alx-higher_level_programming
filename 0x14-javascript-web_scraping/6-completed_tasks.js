c14592b05e734826cc22c3b3a5966d003c8d639e
Jany Muong <me.roumuong@gmail.com>
2023-07-25 18:01:15 +0300

    [fix correction] scrape API url

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,23 +2,32 @@
 
 // computes the number of tasks completed by user id; parse data:
 // API URL: https://jsonplaceholder.typicode.com/todos
+
 const req = require('request');
 
-function todosComplete (apiUrl) {
-  req(apiUrl, (err, res, body) => {
-    if (!err) {
-      const myObj = {};
+function todoComplete (apiUrl) {
+  req(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
       const tasks = JSON.parse(body);
-      for (const task of tasks) {
+      const completedTasks = {};
+
+      tasks.forEach((task) => {
         if (task.completed) {
-          myObj[task.userId] = myObj[task.userId] ? myObj[task.userId] + 1 : 1;
+          if (completedTasks[task.userId]) {
+            completedTasks[task.userId]++;
+          } else {
+            completedTasks[task.userId] = 1;
+          }
         }
-      }
-      console.log(myObj);
+      });
+
+      console.log(completedTasks);
     }
   });
 }
 
 const apiUrl = process.argv[2];
 
-todosComplete(apiUrl);
+todoComplete(apiUrl);
