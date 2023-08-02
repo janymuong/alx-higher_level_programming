cd9c24c8d97396434baffc5e29c67c8e112fb470
Jany Muong <me.roumuong@gmail.com>
2023-08-02 21:48:15 +0300

    JavaScript non-jQuery API: DOMContentLoader

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// 100-script.js:
+// update the text color of the <header> tag to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  const headerEl = document.querySelector('header');
+  headerEl.style.color = '#FF0000';
+});
